8228a60130679b0281d9acca55e9a34846f0db59
azzure277 <brandon.govender21@outlook.com>
2025-08-04 15:45:30 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mssa_5._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 3, 1 };
+            int[] nums2 = { 1, 2, 3, 4 };
+            int[] nums3 = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+            Console.WriteLine(ContainsDuplicate(nums1)); // Output: True
+            Console.WriteLine(ContainsDuplicate(nums2)); // Output: False
+            Console.WriteLine(ContainsDuplicate(nums3)); // Output: True
+        }
+
+        static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> seen = new HashSet<int>();
+            foreach (int num in nums)
+            {
+                if (!seen.Add(num))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
